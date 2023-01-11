c5a3795dace362c4e4a89504b0e4f0a816434fcf
alex-h-vierra <112529361+alex-h-vierra@users.noreply.github.com>
2023-01-11 09:57:28 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <iomanip>
+
+#define ROW 5
+#define COL 5
+int CHESSBOARD[ROW][COL];
+
+bool moveKnight(int row, int col, int moveNumber);
+void printChessBoard();
+
+int main()
+{
+    moveKnight(1, 1, 1);
+    printChessBoard();
+}
+
+bool moveKnight(int row, int col, int moveNumber)
+{
+    int moveTries = 0;
+    if (moveNumber == ROW * COL)
+    {
+        return true;
+    }
+    if (row < ROW && col < COL, row >= 0, col >= 0 && CHESSBOARD[row][col] == 0)
+    {
+        return true;
+    }
+    else if (CHESSBOARD[row][col] == 0)
+    {
+
+    }
+    if (moveKnight(row + 1, col + 2, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row + 2, col + 1, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row + 2, col - 1, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row + 1, col - 2, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row - 1, col - 2, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row - 2, col - 1, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row - 2, col + 1, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else if (moveKnight(row + 2, col - 1, moveNumber + 1))
+    {
+        moveTries + 1;
+        return true;
+    }
+    else
+    {
+        CHESSBOARD[row][col] = 0;
+        return false;
+    }
+}
+
+void printChessBoard()
+{
+    for (int y = 0; y < ROW; y++)
+    {
+        std::cout << std::endl;
+        for (int x = 0; x < COL; x++)
+        {
+            std::cout << std::setw(3) << CHESSBOARD[y][x];
+        }
+    }
+}
+
