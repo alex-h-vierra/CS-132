83b4db2718cf4119d2a7b182295ed612ce05f503
alex-h-vierra <112529361+alex-h-vierra@users.noreply.github.com>
2023-01-14 23:43:38 -0800

    Update KnightTour.cpp

--- KnightTour.cpp
+++ KnightTour.cpp
@@ -1,22 +1,19 @@
 /*
+ * @author Alex Vierra
+ * Section A
  * Knight Tour Program
- * This program with complete the knight tour by visiting 
- * all the places on a desired board noting the spots it has visited 
- * @author Alex Vierra 
+ * This program will complete the knight tour by visiting
+ * all the places on a desired board noting the spots it has visited
+ * with the user input the starting position are row, and col, and the output will be
+ * the chessboard with number indicating legal moves as well as how many tries it took.
  * @version CS 132 1/12/2023 1.0
  */
 #include <iostream>
 #include <iomanip>
+#include "Function prototypes.h"
 
-//Board size control
-const int ROW = 8;
-const int COL = 8;
 int CHESSBOARD[ROW][COL];
 int NUMBER_OF_MOVES = 0;
-
-bool moveKnight(int row, int col, int moveNumber);
-
-void printChessBoard();
 
 int main()
 {
@@ -26,42 +23,32 @@ int main()
     std::cout << std::endl << std::setw(13) << "< " << NUMBER_OF_MOVES << " > ";
 }
 
-/*
- * This will execute how many tries it takes to complete the knight tour
- * #param row: starting position 
- * #param col: starting position
- * #param moveNumber: placing starting move 
- * preconditions: this will calculate how many moves it takes to solve for a 8x8 board
- * post-conditions: print the complete  solved chessboard, and print how many tries it took
- * return true: is if Goal is met or if row and col pass condition
- * return false: if the row or col went out of bounds or the chessboard spot is taken or can't complete the tour
- */
 bool moveKnight(int row, int col, int moveNumber)
 {
-    //checks to see if it's in the bounders  
+    //checks to see if it's in the bounders
     if (row >= ROW || col >= COL || row < 0 || col < 0)
     {
         return false;
     }
-    //checks to see if board has an open spot 
+    //checks to see if board has an open spot
     if (CHESSBOARD[row][col] != 0)
     {
         return false;
     }
-    // checks if moveNumber has made it to end condition 
+    // checks if moveNumber has made it to end condition
     if (moveNumber == ROW * COL)
     {
         CHESSBOARD[row][col] = moveNumber;
         return true;
     }
-    
-    //prints to board once row and col have passed all the conditions 
+
+    //prints to board once row and col have passed all the conditions
     CHESSBOARD[row][col] = moveNumber;
-    
-    //increment tries 
+
+    //increment chess piece tries
     NUMBER_OF_MOVES++;
-    
-    //The move Types 
+
+    //The move Types
     if (moveKnight(row + 1, col + 2, moveNumber + 1))
     {
         return true;
@@ -94,8 +81,7 @@ bool moveKnight(int row, int col, int moveNumber)
     {
         return true;
     }
-    
-    //if all fail set current position back to 0 and go through move list again
+        //if all fail set current position back to 0 and go through move list again
     else
     {
         CHESSBOARD[row][col] = 0;
@@ -106,16 +92,29 @@ bool moveKnight(int row, int col, int moveNumber)
 
 /*
  * This will print the board once recursive function is done
- * return once chessboard is done printing 
+ * return once chessboard is done printing
  */
 void printChessBoard()
 {
-    for (auto & y : CHESSBOARD)
+    for (auto &y: CHESSBOARD)
     {
         std::cout << std::endl;
-        for (int x = 0; x < COL; x++)
+        for (int x : y)
         {
-            std::cout << " " << std::setw(2) << y[x] << " ";
+            std::cout << " " << std::setw(2) << x << " ";
         }
     }
 }
+
+            //output from Knight Tour project
+        // 1  38  59  36  43  48  57  52
+        //60  35   2  49  58  51  44  47
+        //39  32  37  42   3  46  53  56
+        //34  61  40  27  50  55   4  45
+        //31  10  33  62  41  26  23  54
+        //18  63  28  11  24  21  14   5
+        //9  30  19  16   7  12  25  22
+        //64  17   8  29  20  15   6  13
+        //
+        //| Number of tries |
+        //< 8250732 >
